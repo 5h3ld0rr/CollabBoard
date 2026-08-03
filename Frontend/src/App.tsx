f30b739a8fa172eec8a6cf5555cb57f4a0349cb5
5h3ld0r <chirathtimodh@gmail.com>
2026-08-03 10:15:34 +0530

    feat(m1): add initial user login page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Navigate to="/register" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
